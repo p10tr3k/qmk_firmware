4615687b4853151dd55f7a893d68b29529ddd4f5
P10tr3k <pgoral@gmail.com>
2026-01-20 17:38:19 +0100

    wip

--- keyboards/keychron/k9_pro/ansi/white/keymaps/p10tr3k/keymap.c
+++ keyboards/keychron/k9_pro/ansi/white/keymaps/p10tr3k/keymap.c
@@ -42,6 +42,7 @@ enum layers {
 #define HM_SCLN RCTL_T(KC_SCLN) // ;: tap ;, hold Right Ctrl
 
 #define SYM_SPC LT(SYM, KC_SPC)
+#define FN_SPC LT(L_FN1, KC_SPC)
 #define NAV_ESC LT(NAV, KC_ESC)
 #define NUM_V   LT(NUM, KC_V)
 
@@ -96,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
         KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                SYM_SPC,                                KC_RALT, MO(WIN_FN),MO(L_FN1),KC_RCTL),
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                FN_SPC,                                 KC_RALT, MO(WIN_FN),MO(L_FN1),KC_RCTL),
 
     [MAC_FN] = LAYOUT_61_ansi(
         KC_GRV,   KC_BRID,  KC_BRIU,  KC_MCTL,  KC_LPAD,  BL_DOWN,  BL_UP,    KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,  _______,
@@ -137,9 +138,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [NUM] = LAYOUT_61_ansi(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  KC_MINS,  KC_7,      KC_8,    KC_9,     _______,  _______,  _______,  _______,
+        _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  KC_MINS,  KC_7,      KC_8,    KC_9,     XXXXXXX,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  KC_PLUS,  KC_4,      KC_5,    KC_6,     KC_DOT,   _______,            _______,
-        _______,            _______,  _______,  _______,  _______,  KC_ASTR,  KC_1,      KC_2,    KC_3,     _______,  _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  KC_ASTR,  KC_1,      KC_2,    KC_3,     KC_DOT,   _______,            _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 };
