26e6e4d6bcd765b395480c6b8fe01aa04465791a
P10tr3k <pgoral@gmail.com>
2026-02-02 23:16:34 +0100

    wip

--- keyboards/keychron/k9_pro/ansi/white/keymaps/p10tr3k/keymap.c
+++ keyboards/keychron/k9_pro/ansi/white/keymaps/p10tr3k/keymap.c
@@ -49,14 +49,25 @@ enum layers {
 #define HG_G MT(MOD_HYPR, KC_G)
 #define HG_H MT(MOD_HYPR, KC_H)
 
+// Tune this window (ms) for what you consider "two quick presses"
+#ifndef BSPC_OPTW_TERM
+#define BSPC_OPTW_TERM 200
+#endif
+
 // Transparent helper
 #define _______ KC_TRNS
 #define I______I /* one*/
 // Custom keycodes for Cmd+C / Cmd+V
 enum custom_keycodes {
-    CMD_C = SAFE_RANGE,
+    CMD_C = NEW_SAFE_RANGE,
     CMD_V,
+    BSPC_OPTW, // Backspace, but double-press => Option+Backspace on the second press
 };
+
+
+static uint16_t last_bspc_time = 0;
+static bool bspc_is_registered = false;
+
 
 // Macros for Cmd+C and Cmd+V on macOS
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -79,6 +90,28 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 unregister_mods(MOD_BIT(KC_LGUI));
             }
             return false;
+        case BSPC_OPTW:
+            if (record->event.pressed) {
+                uint16_t elapsed = timer_elapsed(last_bspc_time);
+
+                if (elapsed < BSPC_OPTW_TERM) {
+                    // Second quick press: send Option+Backspace immediately as a tap
+                    tap_code16(LALT(KC_BSPC));
+                    bspc_is_registered = false;
+                } else {
+                    // First press: behave like a real held Backspace (supports key repeat)
+                    register_code(KC_BSPC);
+                    bspc_is_registered = true;
+                }
+
+                last_bspc_time = timer_read();
+            } else {
+                if (bspc_is_registered) {
+                    unregister_code(KC_BSPC);
+                    bspc_is_registered = false;
+                }
+            }
+            return false;
     }
     return true;
 }
@@ -93,9 +126,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               SYM_SPC,                                KC_RCMMD,MO(MAC_FN),MO(L_FN1),KC_RCTL),
 
     [WIN_BASE] = LAYOUT_61_ansi(
-        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
+        KC_ESC,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSLS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSPC,
+        NAV_ESC,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            KC_ENT,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,
         KC_LCTL,  KC_LWIN,  KC_LALT,                                FN_SPC,                                 KC_RALT, MO(WIN_FN),MO(L_FN1),KC_RCTL),
 
@@ -141,6 +174,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  KC_MINS,  KC_7,      KC_8,    KC_9,     XXXXXXX,  _______,  _______,  _______,
         _______,  _______,  _______,  _______,  _______,  _______,  KC_PLUS,  KC_4,      KC_5,    KC_6,     KC_DOT,   _______,            _______,
-        _______,            _______,  _______,  _______,  _______,  KC_ASTR,  KC_1,      KC_2,    KC_3,     KC_DOT,   _______,            _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  KC_ASTR,   KC_1,    KC_2,     KC_3,     KC_DOT,             _______,
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,  _______),
 };
